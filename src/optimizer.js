f6e3e2f12f38adee57dc0fa1746e0b4665e50792
Ray Toal <rtoal@lmu.edu>
2021-02-16 00:33:21 -0800

    Use ===

--- src/optimizer.js
+++ src/optimizer.js
@@ -85,17 +85,17 @@ const optimizers = {
           return e.left / e.right
         } else if (e.op === "**") {
           return e.left ** e.right
-        } else if (e.op == "<") {
+        } else if (e.op === "<") {
           return e.left < e.right
-        } else if (e.op == "<=") {
+        } else if (e.op === "<=") {
           return e.left <= e.right
-        } else if (e.op == "==") {
+        } else if (e.op === "==") {
           return e.left === e.right
-        } else if (e.op == "!=") {
+        } else if (e.op === "!=") {
           return e.left !== e.right
-        } else if (e.op == ">=") {
+        } else if (e.op === ">=") {
           return e.left >= e.right
-        } else if (e.op == ">") {
+        } else if (e.op === ">") {
           return e.left > e.right
         }
       } else if (e.left === 0 && e.op === "+") {
