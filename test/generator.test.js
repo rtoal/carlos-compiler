f9f6c61cec9d9f6c511d766a364c6f94e0b630a8
Ray Toal <rtoal@lmu.edu>
2021-02-21 16:30:14 -0800

    Format tests

--- test/generator.test.js
+++ test/generator.test.js
@@ -15,7 +15,8 @@ const fixtures = [
       let x = 3.1 * 7
       let y = true
       y = 5 ** -x / -100 > - x || false
-      print (y && y) || false || (x*2) != 5`,
+      print (y && y) || false || (x*2) != 5
+    `,
     expected: dedent`
       let x_1 = 21.7;
       let y_2 = true;
@@ -26,12 +27,12 @@ const fixtures = [
   {
     name: "iffy",
     source: `
-    let x = 0
-    if (x == 0) { print 1 }
-    if (x == 0) { print 1 } else { print 2 }
-    if (x == 0) { print 1 } else if (x == 2) { print 3 }
-    if (x == 0) { print 1 } else if (x == 2) { print 3 } else { print 4 }
-  `,
+      let x = 0
+      if (x == 0) { print 1 }
+      if (x == 0) { print 1 } else { print 2 }
+      if (x == 0) { print 1 } else if (x == 2) { print 3 }
+      if (x == 0) { print 1 } else if (x == 2) { print 3 } else { print 4 }
+    `,
     expected: dedent`
       let x_1 = 0;
       if ((x_1 === 0)) {
@@ -56,20 +57,22 @@ const fixtures = [
       console.log(3);
       } else {
       console.log(4);
-      }`,
+      }
+    `,
   },
   {
     name: "whiley",
     source: `
-    let x = 0
-    while x < 5 {
-      let y = 0
-      while y < 5 {
-        print x * y
-        y = y + 1
+      let x = 0
+      while x < 5 {
+        let y = 0
+        while y < 5 {
+          print x * y
+          y = y + 1
+        }
+        x = x + 1
       }
-      x = x + 1
-    }`,
+    `,
     expected: dedent`
       let x_1 = 0;
       while ((x_1 < 5)) {
@@ -79,7 +82,8 @@ const fixtures = [
       y_2 = (y_2 + 1);
       }
       x_1 = (x_1 + 1);
-      }`,
+      }
+    `,
   },
 ]
 
